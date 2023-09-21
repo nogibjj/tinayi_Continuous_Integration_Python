04490cc06fc9002d0f318b173232dbaa77165320
tinayiluo0322 <143360909+tinayiluo0322@users.noreply.github.com>
2023-09-21 00:12:44 +0000

    update summary graphs and reports

--- script.py
+++ script.py
@@ -82,7 +82,7 @@ def save_to_markdown(csv):
 
     # Write the markdown report to a file
     with open("heart_summary.md", "w", encoding="utf-8") as file:
-        file.write("## Describe:\n")
+        file.write("## Summary Statistics:\n")
         file.write(markdown_table1)
         file.write("\n\n")  # Add a new line
         file.write("## Median:\n")
